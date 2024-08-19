1f34f63641e2915f1bb909e049b9ea53a64d8880
Facundo nelson <facundo.nelson@fluxit.com.ar>
2024-08-19 15:26:11 -0300

    add section 2

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,9 +1,27 @@
 import homePage from "../pages/homePage";
 
 describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
-    cy.wait(500);
-    homePage.clickOnUtilities();
+  //Correr siempre antes de de la ejeucion de un caso
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+    cy.wait(500)
+  })
+
+  //corroborar mediante un  elemento por una etiqueta  y un assert
+  it("check element by tag", () => {
+    cy.get("h1").contains("Testing Next.js Applications with Cypress")
   });
+
+  //corroborar mediante un commando usando un  atributo (puede ser id , class , data-cy ,etc)  y un assert
+  it("check element by data-test", () => {
+     cy.getByData("hero-heading").contains("Testing Next.js Applications with Cypress")
+  });
+
+  //Corroborar un elemento en una lista por posicion 
+  it("check elements  in list ", () => {
+    cy.get("dt").eq(0).contains("4 Courses")
+    cy.get("dt").eq(1).contains("25+ Lessons")
+    cy.get("dt").eq(2).contains("Free and Open Source")
+  });
+
 });
