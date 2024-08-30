f130210e4575c1fd5f71aac90eecf4b5c45e883e
Facundo nelson <facundo.nelson@fluxit.com.ar>
2024-08-30 12:06:53 -0300

    add section 4

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -12,17 +12,45 @@ describe("template spec", () => {
     cy.get("h1").contains("Testing Next.js Applications with Cypress")
   });
 
+
+  //Context realiza un subconjunto de casos para poder agrupar por casos 
+  context("initial section", () =>{
   //corroborar mediante un commando usando un  atributo (puede ser id , class , data-cy ,etc)  y un assert
-  it("check element by data-test", () => {
-     cy.getByData("hero-heading").contains("Testing Next.js Applications with Cypress")
-  });
+    it("check element by data-test", () => {
+      cy.getByData("hero-heading").contains("Testing Next.js Applications with Cypress")
+    });
 
   //Corroborar un elemento en una lista por posicion 
-  it("check elements  in list ", () => {
-    cy.get("dt").eq(0).contains("4 Courses")
-    cy.get("dt").eq(1).contains("25+ Lessons")
-    cy.get("dt").eq(2).contains("Free and Open Source")
-  });
+    it("check elements  in list ", () => {
+      cy.get("dt").eq(0).contains("4 Courses")
+      cy.get("dt").eq(1).contains("25+ Lessons")
+      cy.get("dt").eq(2).contains("Free and Open Source")
+    });
+  })
+
+
+  context("Testing Your First Next.js Application section " , () =>{
+    it("course : Testing Your First Next.js Application", () => {
+      cy.getByData("course-0").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal","/testing-your-first-application")
+    })
+  })
+
+
+  context("Testing Foundations section " , () =>{
+    it("course : Testing Foundations section", () => {
+      cy.getByData("course-1").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal","/testing-foundations")
+    })
+  })
+
+
+  context("Cypress Fundamentals section " , () =>{
+    it("course : Cypress Fundamentals section", () => {
+      cy.getByData("course-2").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal","/cypress-fundamentals")
+    })
+  })
 
   
 
